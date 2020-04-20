4dbd2f120557019442eba0ce775b441b75c4272d
Tom Konijn <tom@ambrero.nl>
2020-04-20 13:55:03 +0200

    ad

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(konijn.it www.konijn.it)
+domains=(konijn.it)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="cert@konijn.it" # Adding a valid address is strongly recommended
